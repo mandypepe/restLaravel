9280d7adfbe66fd10b861880ca82a12651514d52
Armando Serrano <you@example.com>
2018-11-29 21:27:23 -0500

    php artisan ide-helper:generate

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2018-11-30 02:18:44.
+ * Generated for Laravel 5.5.44 on 2018-11-30 02:25:42.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
