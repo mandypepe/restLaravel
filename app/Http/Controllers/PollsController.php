1fdbd9f2eaf7dc3de052e6ca55b6ee9dce511ed0
Armando Serrano <you@example.com>
2018-12-01 00:19:14 -0500

    recursos

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Poll;
 use Illuminate\Http\Request;
 use Validator;
+use App\Http\Resources\Poll as PollResource;
 
 class PollsController extends Controller
 {
@@ -15,38 +16,41 @@ class PollsController extends Controller
 
     public function show($id)
     {
-        $poll=Poll::find($id);
-            if (is_null($poll))
-            {
-                return response()->json(null,404);
-            }
-        return response()->json(Poll::findOrFail($id), 200);
+        $poll = Poll::find($id);
+        if (is_null($poll)) {
+            return response()->json(null, 404);
+        }
+        //return response()->json(Poll::findOrFail($id), 200);
+        $response = new PollResource(Poll::findOrFail($id), 200);
+        return response()->json($response, 404);
     }
 
     public function store(Request $request)
     {
-        $rules=['title'=>'required|max:10'];
-        $validator= Validator::make($request->all(), $rules);
+        $rules = ['title' => 'required|max:10'];
+        $validator = Validator::make($request->all(), $rules);
         //dd($validator->fails());
-        if ($validator->fails())
-        {
-            return response()->json($validator->errors(),404);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 404);
         }
         $poll_new = Poll::create($request->all());
         return response()->json($poll_new, 201);
     }
-    public function update(Request $request,Poll $poll)
+
+    public function update(Request $request, Poll $poll)
     {
         $poll->update($request->all());
         return response()->json($poll, 200);
     }
-    public function delete(Request $request,Poll $poll)
+
+    public function delete(Request $request, Poll $poll)
     {
         $poll->delete();
         return response()->json(null, 204);
     }
-    public function  errors()
+
+    public function errors()
     {
-        return response()->json(['msg'=>'Esto es esparta'],501);
+        return response()->json(['msg' => 'Esto es esparta'], 501);
     }
 }
