62b5c06f5e798a7ee9f642ea5eba2e96aab99f69
Armando Serrano <you@example.com>
2018-12-02 15:30:03 -0500

    LOGS

---
+++ app/Http/Middleware/Loggin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+
+
+class Loggin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::debug(request()->method());
+        return $next($request);
+    }
+    public function terminate($resquest,$response)
+    {
+        Log::debug($response->status());
+    }
+}
